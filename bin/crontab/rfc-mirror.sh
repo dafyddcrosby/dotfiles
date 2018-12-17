4c278f827a99a6802f0b3bb48f333e1b784e7317
David T. Crosby <dtcrsby@gmail.com>
2018-12-17 08:22:26 -0700

    Update rfc-mirror.sh

--- bin/crontab/rfc-mirror.sh
+++ bin/crontab/rfc-mirror.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p ~/rfc-mirror
 mkdir -p ~/rfc-mirror-ids
-rsync -avz --delete ftp.rfc-editor.org::rfcs-text-only ~/rfc-mirror
+rsync -avzq --delete ftp.rfc-editor.org::rfcs-text-only ~/rfc-mirror
 rsync -avzq --ignore-existing ftp.rfc-editor.org::ids-text-only ~/rfc-mirror-ids
