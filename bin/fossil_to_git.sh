070efd74e25bff92afe706f82f15c3815540c343
David Crosby <dave@dafyddcrosby.com>
2022-08-28 08:14:21 -0600

    Fossil repo to git repo export helper script

---
+++ bin/fossil_to_git.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+GIT_DIR=$(mktemp -d)
+BARE_GIT_DIR=$(mktemp -d)
+cd "$GIT_DIR" || exit
+git init .
+fossil git export . -R "$1" --mainbranch main
+git clone --bare "$GIT_DIR" "$BARE_GIT_DIR"
+echo "New bare directory is $BARE_GIT_DIR"
