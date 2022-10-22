37b601bf30bf8445337fa443dfb671d93f710fc8
David Crosby <dave@dafyddcrosby.com>
2022-10-22 13:23:07 -0600

    Fix redo system, add some key bindings

--- .emacs.d/init/evil.el
+++ .emacs.d/init/evil.el
@@ -1,7 +1,14 @@
+; Fix TAB in emacs
+(setq evil-want-C-i-jump nil)
+(setq evil-undo-system 'undo-redo) ; Emacs 28+
+
 (require 'evil)
 (evil-mode 1)
 (evil-set-initial-state 'help-mode 'emacs)
-(setq evil-undo-system 'undo-redo) ; Emacs 28+
+
+(evil-define-key 'normal org-mode-map (kbd "<tab>") #'org-cycle)
+(evil-define-key 'normal org-mode-map (kbd "RET") #'org-open-at-point)
+(evil-define-key nil 'global (kbd "C-S-v") #'yank)
 
 ;; (require 'org-evil)
 
