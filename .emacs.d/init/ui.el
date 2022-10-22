d24ffe9cf7f74be877ba42dd8281b76265a0af44
David Crosby <dave@dafyddcrosby.com>
2022-10-22 13:21:12 -0600

    Make X emacsclient less awful

--- .emacs.d/init/ui.el
+++ .emacs.d/init/ui.el
@@ -1,8 +1,16 @@
 ;; disable the menu bar
 (menu-bar-mode 0)
+(tool-bar-mode 0)
 
 (setq font-lock-maximum-decoration t)
 
 (setq use-short-answers t)
 
 (setq visible-bell t)
+
+(global-set-key (kbd "M-o") 'other-window)
+(global-set-key (kbd "C-x f") 'find-file)
+
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+(auto-fill-mode 1)
+(setq-default fill-column 120)
