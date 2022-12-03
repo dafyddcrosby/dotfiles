cd6810e119d0cbccda7ef073a3b953eb181f17e2
David Crosby <dave@dafyddcrosby.com>
2022-12-02 18:58:07 -0700

    make help a lil' more evil

--- .emacs.d/init/evil.el
+++ .emacs.d/init/evil.el
@@ -4,10 +4,14 @@
 
 (require 'evil)
 (evil-mode 1)
-(evil-set-initial-state 'help-mode 'emacs)
 
 (evil-define-key 'normal org-mode-map (kbd "<tab>") #'org-cycle)
 (evil-define-key 'normal org-mode-map (kbd "RET") #'org-open-at-point)
+
+(evil-define-key 'motion Info-mode-map (kbd "RET") #'Info-follow-nearest-node)
+(evil-define-key 'motion Info-mode-map (kbd "[") #'Info-backward-node)
+(evil-define-key 'motion Info-mode-map (kbd "]") #'Info-forward-node)
+
 (evil-define-key nil 'global (kbd "C-S-v") #'yank)
 
 ;; (require 'org-evil)
