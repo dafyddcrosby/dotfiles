1fb40066007b3367576c3d0e96561f560f1ba5f8
David Crosby <dave@dafyddcrosby.com>
2022-12-02 19:02:29 -0700

    sync

--- .emacs.d/init/ui.el
+++ .emacs.d/init/ui.el
@@ -1,8 +1,16 @@
 ;; disable the menu bar
 (menu-bar-mode 0)
 (tool-bar-mode 0)
+(scroll-bar-mode 0)
+(horizontal-scroll-bar-mode 0)
+
+; unique buffer names
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
 
 (setq font-lock-maximum-decoration t)
+
+(setq apropos-do-all t)
 
 (setq use-short-answers t)
 
@@ -13,6 +21,8 @@
 (global-set-key (kbd "M-o") 'other-window)
 (global-set-key (kbd "C-x f") 'find-file)
 
-(add-hook 'text-mode-hook 'turn-on-auto-fill)
-(auto-fill-mode 1)
-(setq-default fill-column 120)
+; swap to make regex default
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
