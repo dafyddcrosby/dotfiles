e3f28347c8cdbd017a7477bbcd694609e684ac0f
David Crosby <dave@dafyddcrosby.com>
2023-01-02 13:12:06 -0700

    Remove main branch flag

--- bin/fossil_to_git.sh
+++ bin/fossil_to_git.sh
@@ -3,6 +3,6 @@ GIT_DIR=$(mktemp -d)
 BARE_GIT_DIR=$(mktemp -d)
 cd "$GIT_DIR" || exit
 git init .
-fossil git export . -R "$1" --mainbranch main
+fossil git export . -R "$1" # --mainbranch main
 git clone --bare "$GIT_DIR" "$BARE_GIT_DIR"
 echo "New bare directory is $BARE_GIT_DIR"
