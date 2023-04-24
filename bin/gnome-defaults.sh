2a4b2d2bdd744620777fda84b9b00d9ea9687d17
David Crosby <dave@dafyddcrosby.com>
2023-04-24 07:10:55 -0600

    Create XKB keyboard variant as a workaround to Colemak caps lock bug

--- bin/gnome-defaults.sh
+++ bin/gnome-defaults.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 # Script to set gnome defaults to what I like
 
-# Turn caps lock into control key
-dconf write /org/gnome/desktop/input-sources/xkb-options "['ctrl:nocaps']"
+settings set org.gnome.desktop.input-sources show-all-sources true
+
+# Turn caps lock into control key (see `man xkeyboard-config`)
+# A note - this is fine with QWERTY, but Colemak in xkb turns caps lock into
+# backspace in addition to control, which is awful. I'll see about upstreaming
+# fixes into xkeyboard-config, but I've already spent enough of my life on this
+# dconf write /org/gnome/desktop/input-sources/xkb-options "['ctrl:nocaps']"
 
 # Disable F1 for help menu
 dconf write /org/gnome/settings-daemon/plugins/media-keys/help "@as []"
